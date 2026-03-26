7aef742943018e2e626ddb7a420b3ead0180581e
Nazirul <nazirul.me01c@gmail.com>
2026-03-26 17:43:56 +0600

    fix: disable eslint rule for explicit any in authApi.ts

--- redux/api/authApi.ts
+++ redux/api/authApi.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { getDeviceInfo } from '@/lib/device-info';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../store';
