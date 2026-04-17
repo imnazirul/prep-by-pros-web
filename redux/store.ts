f9b659ebd2c649cdc7acf08cc3a6b52bae02cee3
Nazirul <nazirul.me01c@gmail.com>
2026-04-17 21:15:20 +0600

    style: format code for consistency and readability in store.ts

--- redux/store.ts
+++ redux/store.ts
@@ -1,12 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { authApi } from './api/authApi'
-import { globalApi } from './api/globalApi'
-import authReducer from './features/authSlice'
-import { createLogger } from 'redux-logger'
+import { configureStore } from '@reduxjs/toolkit';
+import { authApi } from './api/authApi';
+import { globalApi } from './api/globalApi';
+import authReducer from './features/authSlice';
+import { createLogger } from 'redux-logger';
 
 export const makeStore = () => {
   const logger = createLogger({
-    collapsed: true, // Collapses the logs by default
+    collapsed: true,
   });
 
   return configureStore({
@@ -16,17 +16,19 @@ export const makeStore = () => {
       [globalApi.reducerPath]: globalApi.reducer,
     },
     middleware: (getDefaultMiddleware) => {
-      const middleware = getDefaultMiddleware().concat(authApi.middleware).concat(globalApi.middleware);
+      const middleware = getDefaultMiddleware()
+        .concat(authApi.middleware)
+        .concat(globalApi.middleware);
       if (process.env.NODE_ENV !== 'production') {
         return middleware.concat(logger);
       }
       return middleware;
     },
-  })
-}
+  });
+};
 
 // Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>
+export type AppStore = ReturnType<typeof makeStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>
-export type AppDispatch = AppStore['dispatch']
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
